5e9e42d3a900e8c1bca7d5a1911edeb75abc0308
Olivia Vidal <olivia.vidal-velazquez@etu.univ-lyon1.fr>
2022-10-22 10:18:32 +0200

    Interface details

--- mastermind/__main__.py
+++ mastermind/__main__.py
@@ -17,11 +17,15 @@ def main():
     round_ = 1.
     win = False
     while (round_<13 and win is not True):
-        print("\nROUND ", round_)
+        print("\nROUND ", int(round_),"/ 12")
         pegs.evolve(win)
         round_ +=1
     if round_==13:
         rprint("[bold red]\nGAME OVER ![/bold red]")
+        print("\nThe right answer was:")
+        answer = [pegs.pegs[f'{pegs.choix[i]}']for i in range(len(pegs.choix))]
+        rprint(' '.join(answer))
+
 
 
 if __name__ == "__main__":
